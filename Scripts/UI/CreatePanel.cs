529818049c56d85fd6af9f1b7997ab79e19a3b45
xueliuxing <2862816634@qq.com>
2019-06-20 11:06:04 +0800

    no message

---
+++ Scripts/UI/CreatePanel.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreatePanel : UIBase
+{
+    private void Awake()
+    {
+        Bind(UIEvent.CREATE_PANEL_ACTIVE);
+    }
+
+    public override void Execute(int eventCode, object message)
+    {
+        switch (eventCode)
+        {
+            case UIEvent.CREATE_PANEL_ACTIVE:
+                setPanelActive((bool)message);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private InputField inputName;
+    private Button btnCreate;
+
+    private PromptMsg promptMsg;
+
+    void Start()
+    {
+        inputName = transform.Find("inputName").GetComponent<InputField>();
+        btnCreate = transform.Find("btnCreate").GetComponent<Button>();
+
+        btnCreate.onClick.AddListener(createClick);
+
+        promptMsg = new PromptMsg();
+    }
+
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        btnCreate.onClick.RemoveListener(createClick);
+    }
+
+    private void createClick()
+    {
+        if (string.IsNullOrEmpty(inputName.text))
+        {
+            //非法输入
+            promptMsg.Change("请正确输入您的名称", Color.red);
+            Dispatch(AreaCode.UI, UIEvent.PROMPT_MSG, promptMsg);
+            return;
+        }
+
+        //自身处理：进行一些其他的判断 比如 长度 符号。。。
+
+    }
+
+}
